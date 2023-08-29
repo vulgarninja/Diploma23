5b298057a92b3ca4b6b322e95fa81ed82b595b60
mujohannes <61717026+mujohannes@users.noreply.github.com>
2023-08-29 21:04:54 +1000

    adding firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import "react-bootstrap/Container"
-import "react-bootstrap/Navbar"
-import "react-bootstrap/Nav"
-import './App.css';
-import Container from "react-bootstrap/Container";
+import { FirebaseConfig } from "./config/Config"
+import { initializeApp } from "firebase/app"
+
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import './App.css'
 
 function App() {
+  const FBapp = initializeApp(FirebaseConfig)
   return (
     <div className="App">
       <Navbar>
         <Container>
-          
+          <Navbar.Brand>App</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+          </Nav>
         </Container>
       </Navbar>
       <h1>React app</h1>
---
+++ src/config/Config.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyA0KziJxDjxoqIAZu-d0K6eoA83FsGQqow",
+  authDomain: "classproject2-f9ca7.firebaseapp.com",
+  projectId: "classproject2-f9ca7",
+  storageBucket: "classproject2-f9ca7.appspot.com",
+  messagingSenderId: "424302348224",
+  appId: "1:424302348224:web:3b23db88eb2623003e6a25"
+}
