d24a4c5518c4a56109e97544d828a16ec65353c5
mujohannes <61717026+mujohannes@users.noreply.github.com>
2023-08-29 16:52:28 +1000

    updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import "react-bootstrap/Container"
+import "react-bootstrap/Navbar"
+import "react-bootstrap/Nav"
 import './App.css';
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Container>
+          
+        </Container>
+      </Navbar>
+      <h1>React app</h1>
     </div>
   );
 }
