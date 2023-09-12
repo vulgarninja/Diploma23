c10e6de63b99a834fe4ada2904ba981ba69478b6
johannes <jmuljana@gmail.com>
2023-09-12 21:13:54 +1000

    adding user to firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { FirebaseConfig } from "./config/Config"
 import { initializeApp } from "firebase/app"
+import { getAuth, createUserWithEmailAndPassword} from "firebase/auth"
 import { Routes, Route } from "react-router-dom"
 import { useState } from "react"
 
@@ -12,6 +13,7 @@ import { Signup } from "./pages/Signup"
 
 function App() {
   const FBapp = initializeApp(FirebaseConfig)
+  const FBauth = getAuth()
   // navigation array
   const NavItems = [
     { label: "Home", link: "/" },
@@ -35,6 +37,15 @@ function App() {
   const saySomething = ( word ) => {
     alert( word )
   }
+  // signing up a user
+  const signUp = ( email, password ) => {
+    createUserWithEmailAndPassword( FBauth, email, password )
+    .then( (userCredential) => {
+      // do something
+    })
+    .catch( (error) => console.log(error.message) )
+  }
+
   return (
     <div className="App">
       <Header items={nav} />
@@ -42,7 +53,7 @@ function App() {
         <Route path="/" element={ <Home greeting="Hey you're at home!" /> } />
         <Route path="/about" element={ <About greeting="Hey you, this is about page!" handler={saySomething} /> } />
         <Route path="/contact" element={ <Contact /> } />
-        <Route path="/signup" element={ <Signup/> } />
+        <Route path="/signup" element={ <Signup handler={signUp}/> } />
       </Routes>
     </div>
   );
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,25 +2,89 @@ import Form from "react-bootstrap/Form"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import {useState, useEffect} from 'react'
 
 export function Signup ( props ) {
+  const[username,setUsername] = useState('')
+  const[validusername,setValidusername] = useState(false)
+  const[useremail,setUseremail] = useState('')
+  const[validemail,setValidemail] = useState(false)
+  const[userpassword,setUserpassword] = useState('')
+  const[validpassword,setValidpassword] = useState(false)
+
+  const submitHandler = (evt) => {
+    evt.preventDefault()
+    props.handler( useremail, userpassword )
+  }
+
+  useEffect( () => {
+    if( username.length >= 4 ) {
+      setValidusername(true)
+    }
+    else {
+      setValidusername(false)
+    }
+  }, [username] )
+
+  useEffect( () => {
+    if( useremail.indexOf('@') > 0 ) {
+      setValidemail(true)
+    }
+    else {
+      setValidemail(false)
+    }
+  }, [useremail])
+
+  useEffect( () => {
+    if( userpassword.length >= 8 ) {
+      setValidpassword(true)
+    }
+    else{
+      setValidpassword(false)
+    }
+  }, [userpassword])
+
   return ( 
     <Container>
       <Row>
-        <Col md={{span: 4, offset: 4}}>
-          <Form>
+        <Col md={ {span: 4, offset: 4} }>
+          <Form onSubmit={submitHandler} >
             <Form.Group>
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" name="username" placeholder="username" />
+              <Form.Control 
+                type="text" 
+                name="username" 
+                placeholder="username" 
+                onChange={ (evt) => setUsername(evt.target.value) }
+              />
             </Form.Group>
             <Form.Group>
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" name="email" placeholder="you@example.com" />
+              <Form.Control 
+                type="email" 
+                name="email" 
+                placeholder="you@example.com" 
+                onChange={ (evt) => setUseremail(evt.target.value) }
+              />
             </Form.Group>
             <Form.Group>
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" name="password" placeholder="minimum 8 characters" />
+              <Form.Control 
+                type="password" 
+                name="password" 
+                placeholder="minimum 8 characters" 
+                onChange={(evt) => setUserpassword(evt.target.value) }
+              />
             </Form.Group>
+            <Button 
+              variant="primary" 
+              className="mt-3 w-100" 
+              type="submit"
+              disabled={ (validemail && validpassword && validusername) ? false : true }
+            >
+              Sign up
+            </Button>
           </Form>
         </Col>
       </Row>
