19142aadb6bfb70297293d3683a331734912ce1f
johannes <jmuljana@gmail.com>
2023-09-19 19:47:52 +1000

    adding signin component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { FirebaseConfig } from "./config/Config"
 import { initializeApp } from "firebase/app"
-import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged} from "firebase/auth"
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut, 
+  signInWithEmailAndPassword 
+} from "firebase/auth"
 import { Routes, Route } from "react-router-dom"
 import { useState } from "react"
 
@@ -10,23 +16,14 @@ import { About } from "./pages/About"
 import { Home } from "./pages/Home"
 import { Contact } from "./pages/Contact"
 import { Signup } from "./pages/Signup"
+import { Signout } from "./pages/Signout"
+import { Signin } from "./pages/Signin"
+
 
 function App() {
   const FBapp = initializeApp(FirebaseConfig)
   const FBauth = getAuth()
-  const [ auth, setAuth ] = useState(false)
-  // authentication observer
-  onAuthStateChanged( FBauth, ( user ) => {
-    if( user ) {
-      // currently authenticated
-      setAuth( user )
-      console.log( user )
-    }
-    else {
-      // currently unauthenticated
-      setAuth( false )
-    }
-  })
+  
   // navigation array
   const NavItems = [
     { label: "Home", link: "/" },
@@ -46,7 +43,21 @@ function App() {
 
   // application states
   const [ nav, setNav ] = useState( NavItems )
+  const [ auth, setAuth ] = useState(false)
 
+  // authentication observer
+  onAuthStateChanged( FBauth, ( user ) => {
+    if( user ) {
+      // currently authenticated
+      setAuth( user )
+      setNav( AuthNavItems )
+    }
+    else {
+      // currently unauthenticated
+      setAuth( false )
+      setNav( NavItems )
+    }
+  })
   const saySomething = ( word ) => {
     alert( word )
   }
@@ -59,6 +70,20 @@ function App() {
     .catch( (error) => console.log(error.message) )
   }
 
+  const logOut = () => {
+    signOut(FBauth).then( () => {
+      // user is signed out
+    })
+  }
+
+  const signIn = ( email, password ) => {
+    signInWithEmailAndPassword( FBauth, email, password )
+    .then( () => {
+      // user is signed in
+    } )
+    .catch( (error) => { console.log(error) })
+  }
+
   return (
     <div className="App">
       <Header items={nav} />
@@ -67,6 +92,8 @@ function App() {
         <Route path="/about" element={ <About greeting="Hey you, this is about page!" handler={saySomething} /> } />
         <Route path="/contact" element={ <Contact /> } />
         <Route path="/signup" element={ <Signup handler={signUp}/> } />
+        <Route path="/signout" element={ <Signout handler={logOut} /> } />
+        <Route path="/signin" element={ <Signin handler={signIn} /> } />
       </Routes>
     </div>
   );
---
+++ src/pages/Signin.js
@@ -0,0 +1,66 @@
+import Form from "react-bootstrap/Form"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+import {useState, useEffect} from 'react'
+
+export function Signin( props ) {
+
+  const [email,setEmail] = useState('')
+  const [validemail, setValidemail] = useState(false)
+  const [password,setPassword] = useState('')
+  const [validpassword, setValidpassword ] = useState(false)
+
+  useEffect( () => {
+    if( email.indexOf('@') > 0 ) {
+      setValidemail(true)
+    }
+    else {
+      setValidemail( false )
+    }
+  } , [email])
+
+  useEffect( () => {
+    if( password.length >= 8 ) {
+      setValidpassword( true )
+    }
+    else {
+      setValidpassword( false )
+    }
+  } , [password])
+
+  return(
+    <Container>
+      <Row>
+        <Col md={ {span: 4, offset: 4} }>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email</Form.Label>
+              <Form.Control 
+                type="email" 
+                name="email" 
+                placeholder="you@example.com" 
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Password</Form.Label>
+              <Form.Control 
+                type="password" 
+                name="password" 
+                placeholder="minimum 8 characters" 
+              />
+            </Form.Group>
+            <Button 
+              variant="primary" 
+              className="mt-3 w-100" 
+              type="submit"
+            >
+              Sign in
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  )
+}
