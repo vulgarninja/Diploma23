ac97950fdbc571e602d692ea9c8b2c977e0153d3
johannes <jmuljana@gmail.com>
2023-10-10 18:57:12 +1100

    return a promise in signin

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { FirebaseConfig } from "./config/Config"
 import { initializeApp } from "firebase/app"
-import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
-  onAuthStateChanged, 
-  signOut, 
-  signInWithEmailAndPassword 
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  signInWithEmailAndPassword
 } from "firebase/auth"
 import { Routes, Route } from "react-router-dom"
 import { useState } from "react"
@@ -26,79 +26,85 @@ import { AuthContext } from "./contexts/AuthContext"
 function App() {
   const FBapp = initializeApp(FirebaseConfig)
   const FBauth = getAuth()
-  
+
   // navigation array
   const NavItems = [
     { label: "Home", link: "/" },
     { label: "About", link: "/about" },
-    { label: "Contact", link: "/contact"},
+    { label: "Contact", link: "/contact" },
     { label: "Create Account", link: "/signup" },
-    { label: "Log in", link: "/signin"}
+    { label: "Log in", link: "/signin" }
   ]
   // navigation for authenticated user
-  
+
   const AuthNavItems = [
     { label: "Home", link: "/" },
     { label: "About", link: "/about" },
-    { label: "Contact", link: "/contact"},
-    { label: "Log out", link: "/signout"}
+    { label: "Contact", link: "/contact" },
+    { label: "Log out", link: "/signout" }
   ]
 
   // application states
-  const [ nav, setNav ] = useState( NavItems )
-  const [ auth, setAuth ] = useState(false)
+  const [nav, setNav] = useState(NavItems)
+  const [auth, setAuth] = useState(false)
 
   // authentication observer
-  onAuthStateChanged( FBauth, ( user ) => {
-    if( user ) {
+  onAuthStateChanged(FBauth, (user) => {
+    if (user) {
       // currently authenticated
-      setAuth( user )
-      setNav( AuthNavItems )
+      setAuth(user)
+      setNav(AuthNavItems)
     }
     else {
       // currently unauthenticated
-      setAuth( false )
-      setNav( NavItems )
+      setAuth(false)
+      setNav(NavItems)
     }
   })
-  const saySomething = ( word ) => {
-    alert( word )
+  const saySomething = (word) => {
+    alert(word)
   }
   // signing up a user
-  const signUp = ( email, password ) => {
-    createUserWithEmailAndPassword( FBauth, email, password )
-    .then( (userCredential) => {
-      // do something
-    })
-    .catch( (error) => console.log(error.message) )
+  const signUp = (email, password) => {
+    createUserWithEmailAndPassword(FBauth, email, password)
+      .then((userCredential) => {
+        // do something
+      })
+      .catch((error) => console.log(error.message))
   }
 
   const logOut = () => {
-    signOut(FBauth).then( () => {
+    signOut(FBauth).then(() => {
       // user is signed out
     })
   }
 
-  const signIn = ( email, password ) => {
-    signInWithEmailAndPassword( FBauth, email, password )
-    .then( () => {
-      // user is signed in
-    } )
-    .catch( (error) => { console.log(error) })
+  const signIn = (email, password) => {
+    return new Promise((resolve, reject) => {
+      signInWithEmailAndPassword(FBauth, email, password)
+        .then(() => {
+          // user is signed in
+          resolve(true)
+        })
+        .catch((error) => { 
+          console.log(error) 
+          reject( error.code )
+        })
+    })
   }
 
   return (
     <div className="App">
       <Header items={nav} />
       <AuthContext.Provider value={auth}>
-      <Routes>
-        <Route path="/" element={ <Home greeting="Hey you're at home!" /> } />
-        <Route path="/about" element={ <About greeting="Hey you, this is about page!" handler={saySomething} /> } />
-        <Route path="/contact" element={ <Contact /> } />
-        <Route path="/signup" element={ <Signup handler={signUp}/> } />
-        <Route path="/signout" element={ <Signout handler={logOut} /> } />
-        <Route path="/signin" element={ <Signin handler={signIn} authstate={auth} /> } />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home greeting="Hey you're at home!" />} />
+          <Route path="/about" element={<About greeting="Hey you, this is about page!" handler={saySomething} />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup handler={signUp} />} />
+          <Route path="/signout" element={<Signout handler={logOut} />} />
+          <Route path="/signin" element={<Signin handler={signIn} authstate={auth} />} />
+        </Routes>
       </AuthContext.Provider>
     </div>
   );
