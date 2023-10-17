66d92b425b550723715cf2f34e776f6cd06ca377
johannes <jmuljana@gmail.com>
2023-10-17 21:08:54 +1100

    show items from firestore

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,38 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Card from "react-bootstrap/Card"
+
+import { useState, useEffect} from 'react'
+
 export function Home ( props ) {
-  return ( <h1>{ props.greeting }</h1>)
+  const [ books, setBooks ] = useState([])
+
+  useEffect( () => {
+    setBooks( props.items )
+  }, [props.items])
+
+  // collection
+  const Items = books.map( (item, key) => {
+    return (
+      <Col md="4" key={key}>
+        <Card>
+          <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>
+              By {item.author}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+    )
+  })
+
+  return (
+    <Container>
+      <Row>
+        {Items}
+      </Row>
+    </Container>
+  )
 }
