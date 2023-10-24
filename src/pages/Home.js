0edaf2783e4d1d8703c590eaf2356e2586fc5a9a
johannes <jmuljana@gmail.com>
2023-10-24 20:49:59 +1100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,36 +7,67 @@ import { ref, getDownloadURL } from 'firebase/storage'
 import { useState, useEffect, useContext } from 'react'
 import { StorageContext } from "../contexts/StorageContext"
 
-export function Home ( props ) {
-  const [ books, setBooks ] = useState([])
+export function Home(props) {
+  const [books, setBooks] = useState([])
 
-  const Storage = useContext( StorageContext )
+  const Storage = useContext(StorageContext)
+  console.log( props.items )
+  // useEffect(() => {
+  //   // if( books.length == 0 ) {
+  //   //   setBooks(props.items)
+  //   // }
+  //   console.log( props.items )
+  // }, [props.items])
 
-  useEffect( () => {
-    setBooks( props.items )
-  }, [props.items])
+  // card image component
+  const ItemImage = (props) => {
+    const [image, setImage] = useState()
 
-  // collection
-  const Items = books.map( (item, key) => {
+    // useEffect( () => {
+
+    // }, [props.name, image ] )
+    console.log(props.name)
+    if (image) {
+      return (
+        <Card.Img variant="top" src={image} />
+      )
+    }
+    else {
+      return (
+        <p>Loading...</p>
+      )
+    }
+  }
+
+
+  if (!books) {
+    return null
+  }
+  else {
+    // collection
+    const Items = books.map((item, key) => {
+      return (
+        <Col md="4" key={key}>
+          <Card>
+            <Card.Body>
+              <ItemImage name={item.cover_image} />
+              <Card.Title>{item.book_title}</Card.Title>
+              <Card.Text>
+                By {item.author}
+              </Card.Text>
+              <Card.Text>{item.cover_image}</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      )
+    })
+
     return (
-      <Col md="4" key={key}>
-        <Card>
-          <Card.Body>
-            <Card.Title>{item.book_title}</Card.Title>
-            <Card.Text>
-              By {item.author}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+      <Container>
+        <Row>
+          {Items}
+        </Row>
+      </Container>
     )
-  })
-
-  return (
-    <Container>
-      <Row>
-        {Items}
-      </Row>
-    </Container>
-  )
+  }
 }
