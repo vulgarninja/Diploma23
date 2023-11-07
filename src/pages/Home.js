af22357277ee0bc8e2caff6c7290d6c10212c35f
johannes <jmuljana@gmail.com>
2023-11-07 22:36:56 +1100

    getting image from storage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,71 +3,37 @@ import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
 
-import { ref, getDownloadURL } from 'firebase/storage'
 import { useState, useEffect, useContext } from 'react'
-import { StorageContext } from "../contexts/StorageContext"
+import { ItemImage } from "../components/ItemImage"
 
 export function Home(props) {
-  const [books, setBooks] = useState([])
-
-  const Storage = useContext(StorageContext)
-  console.log( props.items )
-  // useEffect(() => {
-  //   // if( books.length == 0 ) {
-  //   //   setBooks(props.items)
-  //   // }
-  //   console.log( props.items )
-  // }, [props.items])
-
-  // card image component
-  const ItemImage = (props) => {
-    const [image, setImage] = useState()
-
-    // useEffect( () => {
-
-    // }, [props.name, image ] )
-    console.log(props.name)
-    if (image) {
-      return (
-        <Card.Img variant="top" src={image} />
-      )
-    }
-    else {
-      return (
-        <p>Loading...</p>
-      )
-    }
-  }
+  const [books,setBooks] = useState([])
+  
+  useEffect(() => {
+    console.log( props.items )
+    setBooks( props.items )
+  }, [props.items])
 
 
-  if (!books) {
-    return null
-  }
-  else {
-    // collection
-    const Items = books.map((item, key) => {
-      return (
-        <Col md="4" key={key}>
-          <Card>
-            <Card.Body>
-              <ItemImage name={item.cover_image} />
-              <Card.Title>{item.book_title}</Card.Title>
-              <Card.Text>
-                By {item.author}
-              </Card.Text>
-              <Card.Text>{item.cover_image}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      )
-    })
 
-    return (
-      <Container>
-        <Row>
-          {Items}
-        </Row>
-      </Container>
+  const ItemCards = books.map( ( book, itemkey ) => {
+    return(
+      <Col md={4}>
+        <Card key={itemkey}>
+          <ItemImage source={ book.cover_image} />
+          <Card.Body>
+            <Card.Title>{ book.book_title }</Card.Title>
+          </Card.Body>
+        </Card>
+      </Col>
     )
-  }
+  })
+
+  return (
+    <Container>
+      <Row>
+        {ItemCards}
+      </Row>
+    </Container>
+  )
 }
